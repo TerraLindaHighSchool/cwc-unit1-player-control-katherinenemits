e3f4855e4f55de42415e72f9f14a9e72a6924bab
Katherine Nemits <kn00772@srcs.loc>
2020-01-16 14:43:43 -0800

    Challenge 1 Completed

--- New Unity Project/Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ New Unity Project/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpinPropellerX : MonoBehaviour
 {
-    public float rotationSpeed;
+    public float speed = 3000;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class SpinPropellerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(0,0, Time.deltaTime * speed);
     }
 }
